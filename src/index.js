89733ce1169f977ad5dc577b365b123c93ad346f
bastienwcs <bastien.krettly@wildcodeschool.com>
2022-06-17 12:34:52 +0200

    fix config file name

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
-require('./config/db')
+require('./config/mongodb')
 require('dotenv').config()
 const routes = require('./routes/index')
 
